acd318a7207fd80dd0b9e9eb9e04e17d821e64c2
timocapa <timo.capa@gmail.com>
2022-10-20 00:53:23 +0200

    oneplus7tpro: initial AOSPA bringup

--- device.mk
+++ device.mk
@@ -12,17 +12,12 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/mixer_paths.xml
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
-
 # Camera
 PRODUCT_PACKAGES += \
     OnePlusCameraHelper
 
 # Device init scripts
 PRODUCT_PACKAGES += \
-    fstab.qcom \
     fstab.qcom.ramdisk
 
 # Overlays
@@ -50,4 +45,4 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
 # Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
+$(call inherit-product, vendor/oneplus/oneplus7tpro/oneplus7tpro-vendor.mk)
